ed48f2f2103736e66ac3ec8c67f9e963e4c6c0bf
Yevhen-Pashkulskyi <pashkulskyevhen@gmail.com>
2024-03-08 01:19:00 +0200

    realizeited class Binary tree and corrected class node

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,2 +1,60 @@
 public class BinaryTree {
+    Node root;
+
+    public BinaryTree() {
+        this.root = null;
+    }
+
+    public void createTree() {
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
+        Node node6 = new Node(6);
+        Node node7 = new Node(7);
+        Node node8 = new Node(8);
+        Node node9 = new Node(9);
+        Node node10 = new Node(10);
+        Node node11 = new Node(11);
+        Node node12 = new Node(12);
+
+        this.root = node6;
+
+        node6.left = node3;
+        node6.right = node9;
+        node3.left = node2;
+        node3.right = node5;
+        node2.left = node1;
+        node2.right = node4;
+        node9.left = node8;
+        node9.right = node11;
+        node8.left = node7;
+        node8.right = node10;
+        node11.right = node12;
+    }
+
+    public void inorderTraversal(Node node) {
+        if (node != null) {
+            inorderTraversal(node.left);
+            System.out.print(node.value + " ");
+            inorderTraversal(node.right);
+        }
+    }
+
+    public void preorderTraversal(Node node) {
+        if (node != null) {
+            System.out.print(node.value + " ");
+            preorderTraversal(node.left);
+            preorderTraversal(node.right);
+        }
+    }
+
+    public void postorderTraversal(Node node) {
+        if (node != null) {
+            postorderTraversal(node.left);
+            postorderTraversal(node.right);
+            System.out.print(node.value + " ");
+        }
+    }
 }
--- src/Node.java
+++ src/Node.java
@@ -3,9 +3,10 @@ public class Node {
     Node left;
     Node right;
 
-    public Node(int value, Node left, Node right) {
+    public Node(int value) {
         this.value = value;
-        this.left = left;
-        this.right = right;
+        this.left = null;
+        this.right = null;
     }
+
 }
