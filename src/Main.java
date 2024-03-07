e962ff268755437a8e4a2dc4704699fcbfac1ed7
Yevhen-Pashkulskyi <pashkulskyevhen@gmail.com>
2024-03-08 01:22:54 +0200

    realizeited class Main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,15 @@
 public class Main {
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+        tree.createTree();
+        System.out.println("Неупорядкований обхід (Inorder traversal):");
+        tree.inorderTraversal(tree.root);
+        System.out.println();
+        System.out.println("Обхід передупорядкований (Preorder traversal):");
+        tree.preorderTraversal(tree.root);
+        System.out.println();
+        System.out.println("Обхід поступорядкований (Postorder traversal):");
+        tree.postorderTraversal(tree.root);
+        System.out.println();
+    }
 }
